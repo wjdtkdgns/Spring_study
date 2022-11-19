b86f044e00a9d9a2909add177c10a7fc0d9869f9
wjdtkdgns <wjdtkdgns329@naver.com>
2022-11-19 16:53:49 +0900

    mvc 2장 끝

---
+++ mvc/src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,11 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter // lombok
+public class HelloData {
+    private String username;
+    private int age;
+}
