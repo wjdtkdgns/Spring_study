0c5abc9e335b68ad5cf88b668f7c385b1411ee29
wjdtkdgns <wjdtkdgns329@naver.com>
2023-03-26 16:17:18 +0900

    event config 수정

--- slack bot test/src/main/java/com/example/demo/common/event/EventConfig.java
+++ slack bot test/src/main/java/com/example/demo/common/event/EventConfig.java
@@ -2,18 +2,18 @@ package com.example.demo.common.event;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EventConfig {
     @Autowired
-    private ApplicationContext applicationContext;
+    private ApplicationEventPublisher applicationEventPublisher;
 
     @Bean
     public InitializingBean eventsInitializer() {
-        return () -> Event.setPublisher(applicationContext);
+        return () -> Event.setPublisher(applicationEventPublisher);
     }
 
 }
