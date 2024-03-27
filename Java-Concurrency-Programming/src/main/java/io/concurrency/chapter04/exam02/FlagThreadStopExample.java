6a855a9cc54a0043d31efd64bd76561eeeb45584
wjdtkdgns <wjdtkdgns10266@gmail.com>
2024-03-27 23:27:46 +0900

    03/27

--- Java-Concurrency-Programming/src/main/java/io/concurrency/chapter04/exam02/FlagThreadStopExample.java
+++ Java-Concurrency-Programming/src/main/java/io/concurrency/chapter04/exam02/FlagThreadStopExample.java
@@ -14,6 +14,10 @@ public class FlagThreadStopExample {
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }*/
+                // 컨텍스트 스위칭 되면서 아래 쓰레드의 캐싱된 정보가 메모리에 저장될 수 있다
+                // 이때, 컨텍스트 스위칭되며 캐시가 초기화 되었기 때문에, 메모리 값을 읽게 된다
+                // 그러면 running은 false 이므로 무한 루프가 멈춘다
+                // 할 수는 있는데, 그냥 atomic, volatile 사용해라
                 count++;
             }
             System.out.println("Thread 1 종료. Count: " + count);
